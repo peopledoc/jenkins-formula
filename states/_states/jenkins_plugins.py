c4d6a8c210915b69ff18bf0e125933eda43a6cf4
Florent Pigout <florent.pigout@gmail.com>
2015-02-28 14:55:42 +0100

    Enhances install plugins state by get update or install state and updating the changes dict at the end.

--- states/_states/jenkins_plugins.py
+++ states/_states/jenkins_plugins.py
@@ -54,6 +54,28 @@ def _update(name, names=None, updateall=True, **kwargs):
     return ret
 
 
+(
+    IS_INSTALLED,
+    NOT_AVAILABLE
+) = range(2)
+
+
+def _info(short_name):
+
+    # get info
+    _runcli = __salt__['jenkins.runcli']  # noqa
+    stdout = _runcli('list-plugins {0}'.format(short_name))
+
+    # check info
+    RE_INSTALL = '(\w.+?)\s.*\s(\d+.*)'
+    m = re.match(RE_INSTALL, stdout)
+    if not m:
+        return NOT_AVAILABLE, 'Invalid info for {0}: {1}'.format(short_name, stdout)  # noqa
+
+    __, version = m.groups()
+    return IS_INSTALLED, version
+
+
 def installed(name, names=None, **kwargs):
     """Ensures jenkins plugins are present.
 
@@ -63,24 +85,64 @@ def installed(name, names=None, **kwargs):
     names
         The names of specifics plugins to ensure.
     """
+    ret = _update(name, names=names, updateall=False)
 
     if not names:
-        names = [names]
-
-    ret = {
-        'name': name,
-        'changes': {},
-        'result': False,
-        'comment': ''
-    }
+        names = [name]
 
     _runcli = __salt__['jenkins.runcli']
     for short_name in names:
+
+        # just updated
+        if short_name in ret['changes']:
+            continue
+
+        # get info before install
         try:
-            stdout, stderr = _runcli('install-plugin {0}'.format(short_name))
+            status, info = _info(short_name)
         except exc.CommandExecutionError as e:
             ret['comment'] = e.message
             return ret
+
+        # installed
+        if IS_INSTALLED:
+            ret['changes'][short_name] = {
+                'old': info,
+                'new': None,
+            }
+            continue
+
+        # install
+        try:
+            stdout = _runcli('install-plugin {0}'.format(short_name))
+        except exc.CommandExecutionError as e:
+            ret['comment'] = e.message
+            return ret
+
+        # get info
+        try:
+            stdout = _runcli('list-plugins {0}'.format(short_name))
+        except exc.CommandExecutionError as e:
+            ret['comment'] = e.message
+            return ret
+
+        # get info after install
+        try:
+            status, info = _info(short_name)
+        except exc.CommandExecutionError as e:
+            ret['comment'] = e.message
+            return ret
+
+        # invalid info
+        if NOT_AVAILABLE:
+            ret['comment'] = info
+            return ret
+
+        # fresh install
+        ret['changes'][short_name] = {
+            'old': None,
+            'new': info,
+        }
 
     ret['result'] = True
     return ret
